cbda178cc27367dd515ff5dbeb0cbc07de571adc
zoruss73 <125628219+zoruss73@users.noreply.github.com>
2025-03-17 17:05:17 +0800

    Update settings.py

--- expo/settings.py
+++ expo/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-me
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
