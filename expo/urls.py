a2ed8b51b9069dd67d1e436aac512595610889a9
zoruss73 <125628219+zoruss73@users.noreply.github.com>
2025-04-06 04:20:33 +0800

    Update urls.py

--- expo/urls.py
+++ expo/urls.py
@@ -32,3 +32,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('paypal.standard.ipn.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
