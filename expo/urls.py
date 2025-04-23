c48b27a5515afc6c8a5621ec5550a282fbb8815e
zoruss73 <russeljarcia07@gmail.com>
2025-04-23 19:12:11 +0800

    commit

--- expo/urls.py
+++ expo/urls.py
@@ -34,8 +34,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-<<<<<<< HEAD
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 282e737 (try prod)
+
